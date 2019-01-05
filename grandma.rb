62d6fcb2d5ffed4bdf5149acb42f7e740ef6951a
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-05 13:08:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 
 def speak_to_grandma(phrase)
   
-  if phrase == "ANYTHING".upcase
+  if phrase == "anything".upcase
   puts "NO, NOT SINCE 1938!"
   
-  elsif phrase == "string"
+  elsif phrase == " "
   puts "HUH?! SPEAK UP, SONNY!"
   
   elsif phrase == "I LOVE YOU GRANDMA!"
