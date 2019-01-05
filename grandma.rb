f3199b0a39c36a3f0d0e0bace347d0f38e6aedc9
JanCarlos Acevedo <jancarlos92@me.com>
2019-01-05 13:49:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,14 @@
+
 speak_to_grandma(phrase)
 phrase = "hi!"
 if phrase == "how are you?"
   puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "what did you eat today?".upcase
   puts "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+else
+  puts "HUH?! SPEAK UP, SONNY!"
 end
 
 
